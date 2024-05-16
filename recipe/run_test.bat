2cef19e85df01b1eaefcc78b3985df5110f6c453
Silvio Traversaro <silvio@traversaro.it>
2024-05-16 21:43:37 +0200

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,7 +1,7 @@
 setlocal EnableDelayedExpansion
 
 :: Compile example that links ace
-cl.exe /EHsc /DWIN32 /I%PREFIX%\Library\include ACE.lib ace_example.cpp
+cl.exe  /std:c++17 /EHsc /DWIN32 /I%PREFIX%\Library\include ACE.lib ace_example.cpp
 if errorlevel 1 exit 1
 
 :: Run example
